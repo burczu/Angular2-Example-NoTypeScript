733d0e271c70119b251f5ca14d02757a0929ae89
burczu <b.dybowski@gmail.com>
2016-02-23 19:15:23 +0100

    webpack transform added to gulp task

--- app/boot.js
+++ app/boot.js
@@ -2,7 +2,7 @@
  * Created by burczu on 23.01.2016.
  */
 
-import { bootstrap }    from 'angular2/platform/browser'
+import { bootstrap } from 'angular2/platform/browser'
 import { AppComponent } from './app.component'
 
 bootstrap(AppComponent);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,8 +2,36 @@ import gulp from 'gulp';
 import gulpUtil from 'gulp-util';
 import webpack from 'webpack';
 
-gulp.task('webpack', () => {
+gulp.task('webpack', done => {
+  webpack({
+    entry: './app/boot.js',
+    output: {
+      filename: './build/js/app.min.js'
+    },
+    module: {
+      loaders: [
+        { test: /\.js?$/, loader: 'babel', exclude: /node_modules|bower_components/, query: { presets: [ 'es2015' ] } },
+      ]
+    }
+  }, function(error) {
+    var pluginError;
 
+    if (error) {
+      pluginError = new gulpUtil.PluginError('webpack', error);
+
+      if (done) {
+        done(pluginError);
+      } else {
+        gulpUtil.log('[webpack]', pluginError);
+      }
+
+      return;
+    }
+
+    if (done) {
+      done();
+    }
+  });
 });
 
 gulp.task('default', ['webpack']);
